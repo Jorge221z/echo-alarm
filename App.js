8ecb4dc861993396171cea08d42da8fac91cbdbb
Jorge221z <jorgemunozcast12@gmail.com>
2025-11-16 19:39:23 +0100

    Create new screen for tone pool selection

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './src/screens/HomeScreen';
+import TonePoolScreen from './src/screens/TonePoolScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,14 +11,18 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Home'>
-        
-
+          
         <Stack.Screen 
           name="Home" 
           component={HomeScreen}
           options={{ headerShown: false }} 
         />
 
+        <Stack.Screen 
+          name="TonePool" 
+          component={TonePoolScreen}
+          options={{ headerShown: false }} 
+        />
 
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/screens/TonePoolScreen.js
@@ -0,0 +1,56 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'react-native-linear-gradient';
+
+export default function HomeScreen({ navigation }) {
+  return (
+  <LinearGradient
+        colors={['#4A90E2', '#6B5CE7', '#5f61e6ff']}
+        style={{flex: 1}}>
+
+    <View style={styles.container}>
+      <Text style={styles.mainText}>Pool de tonos</Text>      
+
+      <StatusBar style="auto" />
+    </View>
+
+  </LinearGradient>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 100,
+  },
+  mainText: {
+    color: '#FFFFFF',
+    fontSize: 24,
+    fontWeight: '700',
+    textAlign: 'center',
+    letterSpacing: 0.5,
+    textShadowColor: 'rgba(0, 0, 0, 0.15)',
+    textShadowOffset: { width: 0, height: 2 },
+    textShadowRadius: 4,
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.3)',
+    overflow: 'hidden',
+  },
+  secoundaryText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: '700',
+    textAlign: 'center',
+    letterSpacing: 0.5,
+    paddingTop: 30,
+    paddingHorizontal: 20,
+  }
+});
