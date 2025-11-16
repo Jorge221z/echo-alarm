006adb3132fb3d4ed89d0b5ce1866186dd52e63d
Jorge221z <jorgemunozcast12@gmail.com>
2025-11-16 23:27:18 +0100

    Main commit: added most of the mvp features for the HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,24 +1,100 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { LinearGradient } from 'react-native-linear-gradient';
+import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
+import { useState } from 'react';
+
+
 
 export default function HomeScreen({ navigation }) {
+
+  const [wakeTime, setWakeTime] = useState(new Date());
+  const handleTimeChange = (newDate) => {
+    setWakeTime(newDate);
+  }
+
+  // Function to open the time picker
+  const openTimePicker = () => {
+    DateTimePickerAndroid.open({
+      value: wakeTime,   // Current time already set
+      onChange: (event, selectedDate) => {
+        const currentDate = selectedDate || wakeTime;
+        if (selectedDate) { // Only update if a date is selected
+          handleTimeChange(currentDate);
+        }
+      },
+      mode: 'time',
+      is24Hour: true,
+    });
+  }
+
+  const [interval, setInterval] = useState(5);
+  const handleIntervalChange = (text) => {
+    const numericalValue = parseInt(text);
+    if (!isNaN(numericalValue)) {
+      setInterval(numericalValue);
+    } else {
+      setInterval(1); // Default to 1 if input is invalid
+      console.log("Invalid input for interval | Falling back to 1");
+    }
+  }
+
+
+  const [alarmCount, setAlarmCount] = useState(5);
+  const handleAlarmCountChange = (text) => {
+    const numericalValue = parseInt(text);
+    if (!isNaN(numericalValue)) {
+      setAlarmCount(numericalValue);
+    } else {
+      setAlarmCount(1); // Default to 1 if input is invalid
+      console.log("Invalid input for alarm count | Falling back to 1");
+    }
+  }
+
   return (
-  <LinearGradient
-        colors={['#4A90E2', '#6B5CE7', '#5f61e6ff']}
-        style={{flex: 1}}>
+    <LinearGradient
+      colors={['#4A90E2', '#6B5CE7', '#5f61e6ff']}
+      style={{ flex: 1 }}>
 
-    <View style={styles.container}>
-      <Text style={styles.mainText}>Hora de despertar -&gt; 8:00</Text>
+      <View style={styles.container}>
 
-      <Text style={styles.secoundaryText}>Número de alarmas {"\n"}programadas -&gt; 12</Text>
+        <TouchableOpacity onPress={openTimePicker}>
+          <View style={{ height: 20 }}>
+            <Text style={styles.mainText}>Hora de despertar: {wakeTime.toLocaleTimeString()}</Text>
+          </View>
+        </TouchableOpacity>
 
-      
 
-      <StatusBar style="auto" />
-    </View>
+        <View style={{ height: 20 }}>
+          <Text style={styles.secoundaryText}>Alarm count for tomorrow: {alarmCount}</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            onChangeText={handleAlarmCountChange}
+            value={alarmCount.toString()}
+            placeholder="Enter count"
+            placeholderTextColor="#CCCCCC"
+          />
+        </View>
 
-  </LinearGradient>
+        <View style={{ height: 20 }}>
+          <Text style={styles.secoundaryText}>Interval: {interval}</Text>
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            onChangeText={handleIntervalChange}
+            value={interval.toString()}
+            placeholder="Enter interval"
+            placeholderTextColor="#CCCCCC"
+          />
+        </View>
+
+
+
+        <StatusBar style="auto" />
+      </View>
+
+    </LinearGradient>
   );
 }
 
@@ -56,5 +132,17 @@ const styles = StyleSheet.create({
     letterSpacing: 0.5,
     paddingTop: 30,
     paddingHorizontal: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 100,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    marginTop: 10,
+    borderRadius: 10,
+    borderColor: 'rgba(255, 255, 255, 0.3)',
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
   }
 });
