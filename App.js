6d062b6f33c94b349f385c2f42e494ca2d15a6ef
Jorge221z <jorgemunozcast12@gmail.com>
2025-11-16 01:10:26 +0100

    First UI approach: Gradient bg and text styles

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'react-native-linear-gradient';
 
 export default function App() {
   return (
+  <LinearGradient
+        colors={['#4A90E2', '#6B5CE7', '#5f61e6ff']}
+        style={styles.container}>
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+
+  </LinearGradient>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
 });
