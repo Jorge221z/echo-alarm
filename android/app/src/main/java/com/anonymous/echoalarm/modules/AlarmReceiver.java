6a0b146f8d5c66e0b4d002068ebb89cd6d034044
Jorge221z <jorgemunozcast12@gmail.com>
2025-11-29 21:08:57 +0100

    Create first java native module

---
+++ android/app/src/main/java/com/anonymous/echoalarm/modules/AlarmReceiver.java
@@ -0,0 +1,23 @@
+package com.anonymous.echoalarm.modules;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ComponentName;
+
+public class AlarmReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // 1. Create the intent to initialize reproduction system
+        Intent serviceIntent = new Intent(context, AlarmSoundService.class);
+
+        // 2. Recover the URI from the tone that were attached to AlarmSchedulerModule (TONE_URI)
+        String toneUri = intent.getStringExtra("TONE_URI");
+        serviceIntent.putExtra("TONE_URI", toneUri);
+
+        // 3. Start the service in foreground
+        context.startForegroundService(serviceIntent);
+
+    }
+}
