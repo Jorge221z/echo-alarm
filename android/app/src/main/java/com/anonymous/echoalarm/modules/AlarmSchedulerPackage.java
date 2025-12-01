575ebbc1b7aee2a0940a110ff4c69f600135651d
Jorge221z <jorgemunozcast12@gmail.com>
2025-12-01 20:32:01 +0100

    Add register packager

---
+++ android/app/src/main/java/com/anonymous/echoalarm/modules/AlarmSchedulerPackage.java
@@ -0,0 +1,33 @@
+package com.anonymous.echoalarm.modules;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AlarmSchedulerPackage implements ReactPackage {
+
+    @NonNull
+    @Override
+    public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        // Register the Module
+        modules.add(new AlarmSchedulerModule(reactContext));
+
+        return modules;
+    }
+
+    @NonNull
+    @Override
+    public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
